487c7facd27f2f11feae30dd903e129d8efb7fa6
Sachin Gavali <saching@udel.edu>
2018-05-29 10:59:57 -0400

    Added long description for the package

--- setup.py
+++ setup.py
@@ -4,6 +4,11 @@ setup(
   packages = ['pyiptmnet'], # this must be the same as the name above
   version = '0.1.1',
   description = 'Python client for iPTMNet REST API - https://research.bioinformatics.udel.edu/iptmnet/',
+  long_description = "PTMnet is a bioinformatics resource for integrated understanding of protein post-translational "
+                     "modifications (PTMs) in systems biology context. "
+                     "It connects multiple disparate bioinformatics tools and systems of text mining, data mining, "
+                     "analysis and visualization tools, and databases and ontologies into an integrated cross-cutting "
+                     "research resource to address the knowledge gaps in exploring and discovering PTM networks.",
   author = 'Sachn Gavali',
   author_email = 'saching@udel.edu',
   url = 'https://github.com/udel-cbcb/pyiptmnet', # use the URL to the github repo
