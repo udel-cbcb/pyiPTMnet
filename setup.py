b9b4e671c45e3bd78d54726e05dd8cad1b360216
Sachin Gavali <saching@udel.edu>
2018-05-29 10:53:09 -0400

    Updated package ersion

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   author = 'Sachn Gavali',
   author_email = 'saching@udel.edu',
   url = 'https://github.com/udel-cbcb/pyiptmnet', # use the URL to the github repo
-  download_url = 'https://github.com/udel-cbcb/pyiptmnet/archive/0.1.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/udel-cbcb/pyiptmnet/archive/0.1.1.tar.gz', # I'll explain this in a second
   keywords = ['iPTMnet', 'API', 'Client', 'REST-API'], # arbitrary keywords
   classifiers = [],
 )
