7d4d237c05ea8ca63e9daa2db35f9c3d75833333
Sachin Gavali <saching@udel.edu>
2018-05-29 11:02:00 -0400

    Updated version to 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'pyiptmnet',
   packages = ['pyiptmnet'], # this must be the same as the name above
-  version = '0.1.1',
+  version = '0.1.2',
   description = 'Python client for iPTMNet REST API - https://research.bioinformatics.udel.edu/iptmnet/',
   long_description = "PTMnet is a bioinformatics resource for integrated understanding of protein post-translational "
                      "modifications (PTMs) in systems biology context. "
@@ -12,7 +12,7 @@ setup(
   author = 'Sachn Gavali',
   author_email = 'saching@udel.edu',
   url = 'https://github.com/udel-cbcb/pyiptmnet', # use the URL to the github repo
-  download_url = 'https://github.com/udel-cbcb/pyiptmnet/archive/0.1.1.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/udel-cbcb/pyiptmnet/archive/0.1.2.tar.gz', # I'll explain this in a second
   keywords = ['iPTMnet', 'API', 'Client', 'REST-API'], # arbitrary keywords
   classifiers = [],
 )
