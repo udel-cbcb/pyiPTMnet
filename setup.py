6ca7603ef113c9c4517011f13f43489926a81db6
Sachin Gavali <sachinx0e@gmail.com>
2019-10-29 18:28:28 -0400

    Added Citations for underlying data sources

--- setup.py
+++ setup.py
@@ -1,18 +1,32 @@
-from distutils.core import setup
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
 setup(
   name = 'pyiptmnet',
   packages = ['pyiptmnet'], # this must be the same as the name above
-  version = '0.1.4',
+  version = '0.1.5',
   description = 'Python client for iPTMNet REST API - https://research.bioinformatics.udel.edu/iptmnet/',
-  long_description = "PTMnet is a bioinformatics resource for integrated understanding of protein post-translational "
-                     "modifications (PTMs) in systems biology context. "
-                     "It connects multiple disparate bioinformatics tools and systems of text mining, data mining, "
-                     "analysis and visualization tools, and databases and ontologies into an integrated cross-cutting "
-                     "research resource to address the knowledge gaps in exploring and discovering PTM networks.",
+  long_description = long_description,
+  long_description_content_type="text/markdown",
   author = 'Sachn Gavali',
   author_email = 'saching@udel.edu',
   url = 'https://github.com/udel-cbcb/pyiptmnet', # use the URL to the github repo
   download_url = 'https://github.com/udel-cbcb/pyiptmnet/archive/0.1.3.tar.gz', # I'll explain this in a second
   keywords = ['iPTMnet', 'API', 'Client', 'REST-API'], # arbitrary keywords
   classifiers = [],
+  install_requires=[
+   'certifi',
+   'chardet',
+   'idna',
+   'jsonschema',
+   'numpy',
+   'pandas',
+   'python-dateutil',
+   'pytz',
+   'requests',
+   'six',
+   'urllib3'
+  ]
 )
