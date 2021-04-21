1673ad0ead61c05898c24ab85bf42c421ef0af68
Sachin Gavali <saching@udel.edu>
2021-04-21 15:38:07 -0700

    Bumped version to 0.1.7

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open('README.md') as f:
 setup(
   name = 'pyiptmnet',
   packages = ['pyiptmnet'], # this must be the same as the name above
-  version = '0.1.6',
+  version = '0.1.7',
   description = 'Python client for iPTMNet REST API - https://research.bioinformatics.udel.edu/iptmnet/',
   long_description = long_description,
   long_description_content_type="text/markdown",
   author = 'Sachn Gavali',
   author_email = 'saching@udel.edu',
   url = 'https://github.com/udel-cbcb/pyiptmnet', # use the URL to the github repo
-  download_url = 'https://github.com/udel-cbcb/pyiptmnet/archive/0.1.6.tar.gz',
+  download_url = 'https://github.com/udel-cbcb/pyiptmnet/archive/0.1.7.tar.gz',
   keywords = ['iPTMnet', 'API', 'Client', 'REST-API'], # arbitrary keywords
   classifiers = [],
   install_requires=[
